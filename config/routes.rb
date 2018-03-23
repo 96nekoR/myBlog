18cc2bcaa800ef25b9157cf995464d372fc9d382
96nekoR <seed.kira0807@gmail.com>
2018-03-23 18:31:00 +0900

    contactに関するgetやpostメソッドのpathを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'contact' => 'contact#index'
+  post 'contact/confirm' => 'contact#confirm'
+  post 'contact/thanks' => 'contact#thanks'
+
   get 'home/index'
   get 'home/about'
-
+  
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
